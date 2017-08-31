7f1a3cc87ca74afbc6b92f37c0c51ab8319a7479
Len Chang <lamgo.chang@gmail.com>
2017-08-31 15:29:23 +0800

    good writing style for Number, String and Boolean'

--- Course Workspace/app.ts
+++ Course Workspace/app.ts
@@ -1,1 +1,14 @@
 console.log("It works!");
+
+//// string error
+// let myName = 'Len';
+// myName = 28;
+
+// boolean error
+// let hasHobbies = false;
+// hasHobbies = 0;
+
+// Good writing style
+let myName: string = 'Len';
+let myAge: number = 27;
+let hasHobbies: boolean = false;
