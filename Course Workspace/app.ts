764f4feb3016e961f8947e01e2e45539ed381334
Len Chang <lamgo.chang@gmail.com>
2017-09-01 20:26:37 +0800

    func, argument types, function types

--- Course Workspace/app.ts
+++ Course Workspace/app.ts
@@ -40,3 +40,32 @@ console.log(typeof(car));
 car = { brand: "BMW", series: 3};
 console.log(car);
 console.log(typeof(car));
+
+// functions
+function returnMyName(): string{
+    return myName;
+    // return myAge;
+}
+console.log(returnMyName());
+
+// void: doesn't return anything.
+function sayHello(): void{
+    console.log("Hello!");
+    // return myName;
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+    return value1 * value2;
+}
+// console.log(multiply(2, 'Max'));
+console.log(multiply(2, 10));
+
+// function types
+let myMultiply: (a: number, b: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+
+myMultiply = multiply;
+console.log(myMultiply(3, 4));
+
