e72ca2e0097b5e2cb158d91b7508bd09c53a857d
Len Chang <lamgo.chang@gmail.com>
2017-09-01 19:57:01 +0800

    the sample type code of array, typles, enum and any

--- Course Workspace/app.ts
+++ Course Workspace/app.ts
@@ -12,3 +12,31 @@ console.log("It works!");
 let myName: string = 'Len';
 let myAge: number = 27;
 let hasHobbies: boolean = false;
+
+// array
+let hobbies = ["Cooking", "Sports"];
+// hobbies = 100;
+
+// tuples
+// order is important.
+let address: [string, boolean, number] = ["Superstreet", false, 99, 88, 0];
+
+// enum
+enum Color {
+    Gray, // 0
+    Green = 100, // 100
+    Blue // 101
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+console.log(Color.Blue);
+
+// any
+let car: any = "BMW";
+console.log(car);
+console.log(typeof(car));
+
+car = { brand: "BMW", series: 3};
+console.log(car);
+console.log(typeof(car));
